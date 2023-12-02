5c6bf67ef357dcad990381e612e7bdeeec0bc973
pandupan <pangestup60@gmail.com>
2023-12-02 21:45:11 +0700

    [FIX] - Set Hosting

--- components/pages/ProdukPage/DaftarProduk.tsx
+++ components/pages/ProdukPage/DaftarProduk.tsx
@@ -4,7 +4,7 @@ import { LuSearch } from "react-icons/lu";
 const DaftarProduk = () => {
     
   return (
-    <div className="flex flex-col items-center w-full min-h-[100vh] ">
+    <div className="flex flex-col items-center w-full min-h-[100vh] p-16">
       <div className="flex flex-col justify-center items-center w-full gap-6 my-6">
         <h1 className="font-bold text-xl text-zinc-700">Daftar Produk</h1>
         <div className="flex items-center justify-center w-full gap-2 px-4">
